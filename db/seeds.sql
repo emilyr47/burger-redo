c637869981801a5662a8594f5adbe206551a3701
Emily Raper <45806338+emilyr47@users.noreply.github.com>
2019-06-17 16:03:58 -0500

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,6 @@
+-- //This table was the original table I used for testing the app.  It may or may not appear automatically when viewing deployed version.
+
+
 INSERT INTO burgers (burger_name) VALUES ('Sweet Bacon');
 INSERT INTO burgers (burger_name) VALUES ('BLT');
 INSERT INTO burgers (burger_name) VALUES ('Classic with tomato');
